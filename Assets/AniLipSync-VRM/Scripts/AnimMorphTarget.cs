2c64d7487863adfa6f2dfd9635626be2793ce8c1
sh_akira <akira.satoh.sh@gmail.com>
2018-12-02 03:46:40 +0900

    First release

---
+++ Assets/AniLipSync-VRM/Scripts/AnimMorphTarget.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using VRM;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AniLipSync.VRM
+{
+    public class AnimMorphTarget : MonoBehaviour
+    {
+        [Tooltip("aa, E, ih, oh, ou のそれぞれの音素へ遷移する際に、BlendShapeの重みを時間をかけて変化させるためのカーブ")]
+        public AnimationCurve[] transitionCurves = new AnimationCurve[5];
+
+        [Tooltip("カーブの値をBlendShapeに適用する際の倍率")]
+        public float curveAmplifier = 100.0f;
+
+        [Range(0.0f, 100.0f), Tooltip("この閾値未満の音素の重みは無視する")]
+        public float weightThreashold = 2.0f;
+
+        [Tooltip("BlendShapeの重みを変化させるフレームレート")]
+        public float frameRate = 12.0f;
+
+        [Tooltip("BlendShapeの値を変化させるSkinnedMeshRenderer")]
+        public VRMBlendShapeProxy blendShapeProxy;
+
+        [Tooltip("aa, E, ih, oh, ouの順で割り当てるBlendShapeのindex")]
+        private Dictionary<BlendShapeKey, float> defaultValueDictionary = new Dictionary<BlendShapeKey, float> {
+            {new BlendShapeKey(BlendShapePreset.A), 0.0f },
+            {new BlendShapeKey(BlendShapePreset.E), 0.0f },
+            {new BlendShapeKey(BlendShapePreset.I), 0.0f },
+            {new BlendShapeKey(BlendShapePreset.O), 0.0f },
+            {new BlendShapeKey(BlendShapePreset.U), 0.0f },
+        };
+        private List<BlendShapeKey> keyList;
+
+        [Tooltip("OVRLipSyncに渡すSmoothing amountの値")]
+        public int smoothAmount = 100;
+
+        OVRLipSyncContextBase context;
+        OVRLipSync.Viseme previousViseme = OVRLipSync.Viseme.sil;
+        float transitionTimer = 0.0f;
+        float frameRateTimer = 0.0f;
+
+
+
+        void Start()
+        {
+            keyList = new List<BlendShapeKey>(defaultValueDictionary.Keys);
+
+            if (blendShapeProxy == null)
+            {
+                Debug.LogError("VRMBlendShapeProxyが指定されていません。", this);
+            }
+
+            context = GetComponent<OVRLipSyncContextBase>();
+            if (context == null)
+            {
+                Debug.LogError("同じGameObjectにOVRLipSyncContextBaseを継承したクラスが見つかりません。", this);
+            }
+
+            context.Smoothing = smoothAmount;
+        }
+
+        void Update()
+        {
+            if (context == null || blendShapeProxy == null)
+            {
+                return;
+            }
+
+            var frame = context.GetCurrentPhonemeFrame();
+            if (frame == null)
+            {
+                return;
+            }
+
+            transitionTimer += Time.deltaTime;
+
+            // 設定したフレームレートへUpdate関数を低下させる
+            frameRateTimer += Time.deltaTime;
+            if (frameRateTimer < 1.0f / frameRate)
+            {
+                return;
+            }
+            frameRateTimer -= 1.0f / frameRate;
+
+            // すでに設定されているBlendShapeの重みをリセット
+            foreach (var key in keyList)
+            {
+                defaultValueDictionary[key] = 0.0f;
+            }
+
+            // 最大の重みを持つ音素を探す
+            var maxVisemeIndex = 0;
+            var maxVisemeWeight = 0.0f;
+            // 子音は無視する
+            for (var i = (int)OVRLipSync.Viseme.aa; i < frame.Visemes.Length; i++)
+            {
+                if (frame.Visemes[i] > maxVisemeWeight)
+                {
+                    maxVisemeWeight = frame.Visemes[i];
+                    maxVisemeIndex = i;
+                }
+            }
+
+            // 音素の重みが小さすぎる場合は口を閉じる
+            if (maxVisemeWeight * 100.0f < weightThreashold)
+            {
+                transitionTimer = 0.0f;
+            }
+
+            // 音素の切り替わりでタイマーをリセットする
+            if (previousViseme != (OVRLipSync.Viseme)maxVisemeIndex)
+            {
+                transitionTimer = 0.0f;
+                previousViseme = (OVRLipSync.Viseme)maxVisemeIndex;
+            }
+            var visemeIndex = maxVisemeIndex - (int)OVRLipSync.Viseme.aa;
+            if (visemeIndex >= 0)
+            {
+                defaultValueDictionary[keyList[visemeIndex]] = transitionCurves[visemeIndex].Evaluate(transitionTimer) * curveAmplifier;
+            }
+            blendShapeProxy.SetValues(defaultValueDictionary.ToList());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # AniLipSync-VRM
 VRMモデルでリミテッドアニメ風のリップシンクを実現するためのライブラリです。
+[AniLipSync](https://github.com/XVI/AniLipSync)がベースになっています。
+
+# ダウンロード
+[AniLipSync-VRM v1.0.0](https://github.com/sh-akira/AniLipSync-VRM/releases/download/v1.0.0/AniLipSync-VRM.unitypackage)
+
+# 開発環境
+- Windows 10
+- Unity 2017.4.15f1
+
+# 必要なライブラリ
+- [OVRLipSync 1.28.0](https://developer.oculus.com/downloads/package/oculus-lipsync-unity/1.28.0/)
+- [UniVRM 0.45](https://github.com/dwango/UniVRM/releases)
+
+# サンプル
+`Assets/AniLipSync-VRM/Examples/Scenes/AniLipSync-VRM.unity` にサンプルシーンがあります。
+[OVRLipSync 1.28.0](https://developer.oculus.com/downloads/package/oculus-lipsync-unity/1.28.0/)と[UniVRM 0.45](https://github.com/dwango/UniVRM/releases)をインポートし、
+[ニコニ立体ちゃんVRMモデル](https://3d.nicovideo.jp/works/td32797)をダウンロードし、AliciaSolid.vrmを
+Assets/Models/AliciaSolid.vrmにフォルダを作ってコピーしてください。
+サンプルシーンを開きHierarchy上にAliciaSolidのPrefabを配置したら、AniLipSync-VRMのAnim Morph Target (Script)の
+Blend Shape ProxyにAliciaSolidを設定してください。
+実行してマイクに喋ると口が動きます。
+
+# 使い方
+1. [OVRLipSync 1.28.0](https://developer.oculus.com/downloads/package/oculus-lipsync-unity/1.28.0/)をインポート
+2. [UniVRM 0.45](https://github.com/dwango/UniVRM/releases)をインポート
+3. [AniLipSync-VRM.unitypackage](https://github.com/sh-akira/AniLipSync-VRM/releases) をインポート
+4. 好きなVRMモデルをインポート
+5. `Assets/Oculus/LipSync/Prefabs/LipSyncInterface` プレハブをシーンに配置
+6. `Assets/AniLipSync-VRM/Prefabs/AniLipSync-VRM` プレハブをシーンに配置
+7. 好きなVRMモデルのプレハブをシーンに配置
+5. `AniLipSync-VRM` GameObjectの `AnimMorphTarget` の各プロパティをインスペクタで編集（BlendShapeProxyにはシーンに配置したVRMモデルを入れてください）
+
+# AnimMorphTargetの各プロパティの説明
+以下説明は元の[AniLipSync](https://github.com/XVI/AniLipSync)と同じです。
+## Transition Curves
+aa, E, ih, oh, ou のそれぞれの音素へ遷移する際に、BlendShapeの重みを時間をかけて変化させるためのカーブです。
+
+例えば、黙っている状態から aa の音素を検知した場合、Element 0 のカーブに従って時々刻々とBlendShapeの重みを変化させます。aa の状態で ih の音素を検知した場合、Element 2 のカーブに従います。
+
+徐々に重みを増やすことで、ゆるやかに口の形が変化するような表現が可能です。
+
+## Curve Amplifier
+Transition Curveの縦軸の値に倍率をかけます。
+
+Transition Curveの縦軸を 0.0 ～ 1.0 の範囲にしておいて 100 の倍率をかけることで、カーブ編集時の煩雑な操作を省略できます。
+
+## Weight Threashold
+この値より小さな音素の重みを無視します。
+
+小さなノイズ等で口が開いてしまう現象を回避できます。
+
+## Frame Rate
+1秒間にBlendShapeの重みを更新する頻度の設定です。
+
+リミテッドアニメ風の効果を得ることができます。
+
+## Skinned Mesh Renderer
+唇のBlendShapeを持ったSkinnedMeshRendererを指定してください。
+
+## Viseme To Blend Shape
+aa, E, ih, oh, ouの順でBlendShapeのIndexを指定してください。
+
+## Smooth Amount
+OVRLipSyncのSmooth amountの値を設定できます。
+
+# 制限事項
+- 声を入力してから口が動くまでに遅延があります。配信ソフトや動画編集ソフトを使用して音声を250msほど遅延させるとタイミングが合うでしょう。
+- `LowLatencyLipSyncContext`はOVRLipSyncより後に実行する必要があります。AniLipSync.unitypackageのインポートで自動的に設定されますが、スクリプトのみをコピーする際は`Script Execution Order`による設定が必要です。
+
+# ライセンス
+本リポジトリに上がっている部分はMIT Licenseです。LICENSEファイルを参照してください。クレジットを記載することで、営利・非営利を問わず利用いただけます。
+
+OVRLipSyncのライセンスについては、Oculus社のサイトを参照してください。
+
+このライブラリは、[AniLipSync](https://github.com/XVI/AniLipSync/blob/master/LICENSE)をもとに作られています。
